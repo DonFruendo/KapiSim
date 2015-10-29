e5152fe0ac1746736d920d16eeb4a6c82e202318
DonFruendo <tobi_kessi@yahoo.de>
2015-10-29 11:56:14 +0100

    Documentation controller
    JavaDoc für das controller-package eingebaut

--- src/controller/package-info.java
+++ src/controller/package-info.java
@@ -1,7 +1,19 @@
 /**
  * 
+ * 
  */
 /**
+ * The controller-package contains every controller in this game.
+ * 
+ * 
+ * <p>
+ * Currently implemented:
+ * <br>{@link GameController}
+ * <br>{@link MarketController}
+ * <br>{@link PlayerController}
+ * <br>{@link ProductionBuildingController}
+ * 
+ * 
  * @author DonFruendo
  *
  */
