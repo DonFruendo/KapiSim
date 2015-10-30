864f890c8b9a169c772587ca940fd683d7931c69
DonFruendo <tobi_kessi@yahoo.de>
2015-10-30 14:38:49 +0100

    Viehzucht sortiert

--- src/production/Viehzucht.java
+++ src/production/Viehzucht.java
@@ -8,10 +8,10 @@ public class Viehzucht extends ProductionBuildingController {
 
 	public Viehzucht(PlayerController owner) {
 		super(owner);
-		addPossible(ProductType.Ei,		  5);
-		addPossible(ProductType.Huhn,	  9);
-		addPossible(ProductType.Lamm,	405);
 		addPossible(ProductType.Milch,	  5);
+		addPossible(ProductType.Huhn,	  9);
+		addPossible(ProductType.Ei,		  5);
+		addPossible(ProductType.Lamm,	405);
 		addPossible(ProductType.Rind,	225);
 		addPossible(ProductType.Schwein,225);
 		//addPossible(ProductType.Wolle,  3);
