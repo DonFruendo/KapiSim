b3b35260abee462205986c12d0337cb4749fcd32
DonFruendo <tobi_kessi@yahoo.de>
2015-11-20 10:35:52 +0100

    Household Implementation
    Haushalte wurden erstellt.

---
+++ src/population/Household.java
@@ -0,0 +1,23 @@
+package population;
+
+import java.util.ArrayList;
+
+public class Household
+{
+	ArrayList<Consumer> familyMembers;
+	
+	public Household()
+	{
+		familyMembers = new ArrayList<Consumer>();
+	}
+	
+	public void addToHouseHold(Consumer newFamiliyMember)
+	{
+		familyMembers.add(newFamiliyMember);
+	}
+	
+	public ArrayList<Consumer> getFamilyMembers()
+	{
+		return familyMembers;
+	}
+}
